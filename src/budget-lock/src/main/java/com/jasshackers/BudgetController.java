03f74501e7d7f0bbc810dbec4ab64ff8a3fc4226
jnharesa <jnharesa@ncsu.edu>
2026-02-14 13:11:53 -0500

    Skeleton

---
+++ src/budget-lock/src/main/java/com/jasshackers/BudgetController.java
@@ -0,0 +1,5 @@
+package com.jasshackers;
+
+public class BudgetController {
+    
+}
---
+++ src/budget-lock/src/main/java/com/jasshackers/Transaction.java
@@ -0,0 +1,23 @@
+package com.jasshackers;
+
+public class Transaction {
+
+    private double amount;
+    private String category;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
